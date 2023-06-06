a7fe5d95df2af1cbb0486043b1be1765c5169f1d
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-06 15:55:41 +0600

    feat: add new pdf cv to download

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import { useTranslation } from 'react-i18next';
+import { LANGS } from 'components/LangSwitcher/const';
+import { Href } from 'models';
 
 export const Title = (): JSX.Element => {
-  const { t } = useTranslation('title');
+  const { t, i18n } = useTranslation('title');
+  const [EN, RU] = LANGS;
 
   return (
     <section className={styles.container}>
       <h1 className={styles.name}>{t('name')}</h1>
       <h2 className={styles.position}>{t('position')}</h2>
-      <button className={styles.button}>{t('download')}</button>
+      <a
+        className={styles.button}
+        download={t('name')}
+        href={i18n.resolvedLanguage === EN ? Href.enCV : Href.enCV}
+      >
+        {t('download')}
+      </a>
     </section>
   );
 };
--- src/models/enum.ts
+++ src/models/enum.ts
@@ -9,6 +9,7 @@ export const enum Href {
   github = 'https://github.com/lexarudak',
   codewars = 'https://www.codewars.com/users/rsschool_21572a2613fea6c2',
   routine = 'https://github.com/lexarudak/routine/blob/notes/score.md',
+  enCV = '/pdf/Aliaksei Rudak CV en.pdf',
 }
 
 export const enum ExperienceType {
