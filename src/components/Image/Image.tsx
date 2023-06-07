befbde88933ed5c588e534835663b23a89ac12e7
Aliaksei Rudak <lexarudak@gmail.com>
2023-06-07 07:33:10 +0600

    test: test new img loading with bg

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,16 +1,7 @@
-import classNames from 'classnames';
-import React, { useState } from 'react';
+import React from 'react';
 import { ALT, SRC } from './const';
 import styles from './styles.module.scss';
 
 export const Image = (): JSX.Element => {
-  const [isImgLoad, setIsImgLoad] = useState(false);
-  return (
-    <img
-      src={SRC}
-      alt={ALT}
-      className={classNames(styles.img, isImgLoad && styles.active)}
-      onLoad={() => setIsImgLoad(true)}
-    />
-  );
+  return <img src={SRC} alt={ALT} className={styles.img} />;
 };
